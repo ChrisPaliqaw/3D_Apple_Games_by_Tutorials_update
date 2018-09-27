688f4844062fe1eb250504873cf278577154d6d0
Christopher Clark <christophomos@Christophers-MacBook-Air.local>
2018-09-27 19:42:19 +0800

    Swap names of challenge and final folders

--- 02-nodes/final/GeometryTree/GeometryTree/GameViewController.swift
+++ 02-nodes/final/GeometryTree/GeometryTree/GameViewController.swift
@@ -54,26 +54,18 @@ class GameViewController: UIViewController {
     }
     
     func spawnShape() {
+        // 1
         var geometry:SCNGeometry
+        // 2
         switch ShapeType.random() {
-        case .box:
-            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
-        case .sphere:
-            geometry = SCNSphere(radius: 0.5)
-        case .pyramid:
-            geometry = SCNPyramid(width: 1.0, height: 1.0, length: 1.0)
-        case .torus:
-            geometry = SCNTorus(ringRadius: 0.5, pipeRadius: 0.25)
-        case .capsule:
-            geometry = SCNCapsule(capRadius: 0.3, height: 2.5)
-        case .cylinder:
-            geometry = SCNCylinder(radius: 0.3, height: 2.5)
-        case .cone:
-            geometry = SCNCone(topRadius: 0.25, bottomRadius: 0.5, height: 1.0)
-        case .tube:
-            geometry = SCNTube(innerRadius: 0.25, outerRadius: 0.5, height: 1.0)
+        default:
+            // 3
+            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0,
+                              chamferRadius: 0.0)
         }
+        // 4
         let geometryNode = SCNNode(geometry: geometry)
+        // 5
         scnScene.rootNode.addChildNode(geometryNode)
     }
 }
